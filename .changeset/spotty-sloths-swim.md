d4db991dc1dcfd646e7febb47aaf0f6de3279000
Ed Spencer <ed@edspencer.net>
2024-12-02 08:04:04 -0500

    Fix CJS build output

---
+++ .changeset/spotty-sloths-swim.md
@@ -0,0 +1,5 @@
+---
+"read-next": patch
+---
+
+Fix build output for CJS
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -5,28 +5,53 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 export default [
   {
-    input: "./src/index.ts",
-    output: [
-      {
-        file: "./dist/index.esm.js",
-        format: "esm",
-      },
-      {
-        file: "./dist/index.cjs.js",
-        format: "cjs",
-      },
-    ],
-    plugins: [
-      peerDepsExternal(), // Automatically externalize peer dependencies
-      // resolve(), // Resolve modules from node_modules
-      commonjs(), // Convert CommonJS modules to ES6
-      typescript({
-        tsconfig: "./tsconfig.json",
-        exclude: ["src/test/**/*", "src/script/**/*"], // Exclude test files from being bundled
-      }),
-    ],
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.js",
+      format: "esm",
+      sourcemap: true,
+    },
+    plugins: [peerDepsExternal(), typescript({ useTsconfigDeclarationDir: true }), commonjs()],
     watch: {
       include: "src/**",
     },
   },
+  // CommonJS Build
+  {
+    input: "src/index.ts",
+    output: {
+      file: "dist/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    plugins: [peerDepsExternal(), typescript({ useTsconfigDeclarationDir: true }), commonjs()],
+    watch: {
+      include: "src/**",
+    },
+  },
+  // {
+  //   input: "./src/index.ts",
+  //   output: [
+  //     {
+  //       file: "./dist/index.esm.js",
+  //       format: "esm",
+  //     },
+  //     {
+  //       file: "./dist/index.cjs.js",
+  //       format: "cjs",
+  //     },
+  //   ],
+  //   plugins: [
+  //     peerDepsExternal(), // Automatically externalize peer dependencies
+  //     // resolve(), // Resolve modules from node_modules
+  //     commonjs(), // Convert CommonJS modules to ES6
+  //     typescript({
+  //       tsconfig: "./tsconfig.json",
+  //       exclude: ["src/test/**/*", "src/script/**/*"], // Exclude test files from being bundled
+  //     }),
+  //   ],
+  //   watch: {
+  //     include: "src/**",
+  //   },
+  // },
 ];
